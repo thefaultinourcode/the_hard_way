9164d946babbff99309eca7f13c30e2377c98abd
Leah Ramsier <ler13c@my.fsu.edu>
2016-05-14 02:17:51 -0400

    first commit

---
+++ ex3.py
@@ -0,0 +1,47 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+#print method
+print "I will now count my chickens:"
+
+#prints Hens, performs arithmetic operations in order of 
+#operations (Divides 30 by 6 then adds 25)
+print "Hens", 25 + 30 / 6
+
+#prints String, multiples 25 by 3 takes the modulus of the product
+#subtracts that from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+#order of operations arithmetic operations 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#prints boolean after evaluating comparison operators
+print 3 + 2 < 5 - 7
+
+#prints question, calculates answer
+print "What is 3 + 2?", 3 + 2
+print "What is 5 -7?", 5 - 7
+
+#seeing the numbers makes it easier to visualize why above statement evaluated False
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+#evaluates comparison operator
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+#evaluates to zero... noted
+print 1 / 4
---
+++ ex3float.py
@@ -0,0 +1,47 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+#print method
+print "I will now count my chickens:"
+
+#prints Hens, performs arithmetic operations in order of 
+#operations (Divides 30 by 6 then adds 25)
+print "Hens", 25.0 + 30.0 / 6.0
+
+#prints String, multiples 25 by 3 takes the modulus of the product
+#subtracts that from 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+#order of operations arithmetic operations 
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+#prints boolean after evaluating comparison operators
+print 3.0 + 2.0 < 5.0 - 7.0
+
+#prints question, calculates answer
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+#seeing the numbers makes it easier to visualize why above statement evaluated False
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+#evaluates comparison operator
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
+
+
+print 1.0 / 4.0
